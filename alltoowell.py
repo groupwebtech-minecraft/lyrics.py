82584c4b4521d06082df15339c63ca3f4909afc1
feverfew <jmmidoro123@gmail.com>
2026-04-22 08:16:24 +0800

    Add files via upload

---
+++ alltoowell.py
@@ -0,0 +1,47 @@
+import time
+import sys
+from threading import Thread, Lock
+
+lock = Lock()
+
+def animate_text(text, delay=0.1):
+    with lock:
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print()
+
+def sing_lyric(lyric, delay, speed):
+    time.sleep(delay)
+    animate_text(lyric, speed)
+
+
+def sing_song():
+    lyrics = [
+
+        ("\n""Just between us, I remember it, all too well", 0.08),
+        ("wind in my hair, I was there, I was there", 0.08),
+        ("Down the stairs, I was there, I was there", 0.08),
+        ("Sacred prayer, I was there, I was there", 0.09),
+        ("It was rare, you remember it all too well.", 0.10)
+    ]
+    delays = [0.3, 6.4, 11.5, 16.5, 21.5]
+
+    threads = []
+    for i, lyric in enumerate(lyrics):
+        lyric, speed = lyrics[i]
+        thread = Thread(target=sing_lyric, args=(lyric, delays[i], speed))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    sing_song()
+
+
+
+
